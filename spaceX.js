d7e984e89d75c258097c43354dd3753d9b883081
tylerc108 <103055666+tylerc108@users.noreply.github.com>
2022-07-06 09:20:16 -0400

    chlnge starter codes

---
+++ spaceX.js
@@ -0,0 +1,13 @@
+const url = "https://api.spacexdata.com/v2/launchpads";
+
+d3.json(url).then(receivedData => console.log(receivedData));
+
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[27];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
