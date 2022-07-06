efd8a5b68fb6728890cd25daaa704fa53aa264b0
tylerc108 <103055666+tylerc108@users.noreply.github.com>
2022-07-06 13:01:36 -0400

    d2 progress

--- charts.js
+++ charts.js
@@ -106,5 +106,28 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+    
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: xValues,
+      y: yTicks,
+      text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+      mode: "markers",
+      marker: {
+        color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+        size: [40, 60, 80, 100]
+      }
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures per Sample',
+      showlegend: false,
+      height: 600,
+      width: 600
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
   });
 }
